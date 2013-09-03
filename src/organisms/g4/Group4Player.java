db153472bf628757b3dee3f835bd6b44ad10edbf
Franklin Yang <franklinyang@seas1214.wireless-pennnet.upenn.edu>
2013-09-03 16:45:16 -0400

    Implement initial naive strategy

---
+++ src/organisms/g4/Group4Player.java
@@ -0,0 +1,83 @@
+package organisms.g4;
+import java.awt.Color;
+import java.util.Random;
+
+import organisms.Move;
+import organisms.OrganismsGame;
+import organisms.Player;
+
+public class Group4Player implements Player {
+
+
+	static final String _CNAME = "Group 4 Player";
+	static final Color _CColor = new Color(1.0f, 0.67f, 0.67f);
+	private int state;
+	private OrganismsGame game;
+
+	@Override
+	public void register(OrganismsGame __amoeba, int key) throws Exception {
+		
+		state = 0;
+		this.game = __amoeba;
+		
+	}
+
+	@Override
+	public String name() throws Exception {
+	  return _CNAME;
+	}
+
+	@Override
+	public Color color() throws Exception {
+		return _CColor;
+	}
+
+	@Override
+	public boolean interactive() throws Exception {
+		return false;
+	}
+
+	@Override
+	public int externalState() throws Exception {
+		return state;
+	}
+
+	@Override
+	public Move move(boolean[] food, int[] enemy, int foodleft, int energyleft)
+			throws Exception {
+
+		int maxEnergy = game.M();
+		
+		if(food[1])
+			return new Move(1);
+		
+		if (energyleft > maxEnergy/2) {
+			return new Move(REPRODUCE, NORTH, state);
+      //reproduce N/W (so child moves before parent)
+		}
+
+	    else if (foodleft > 0) {
+	      return new Move(STAYPUT);
+	    }
+	
+	    else {
+	      // inhabit to be true if food is present, enemy is absent
+	      boolean[] inhabit = new boolean[4];
+	      for (int i=0; i < 4; i++) {
+	        inhabit[i] = food[i] && (enemy[i] != -1); 
+	      }
+	
+	      for (int dir=0; dir < 4; dir++) {
+	
+	        if (inhabit[dir]) {
+	          // ***check to see if food array is correctly specified
+	          return new Move(dir+1);
+	        }
+	
+	      }
+	
+	      return new Move(STAYPUT);
+	    }
+	}
+
+}
